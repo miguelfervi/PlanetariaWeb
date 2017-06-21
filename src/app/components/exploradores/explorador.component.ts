1e2c368cebd2a693b303bdba03b5ffdd657239e2
Miguel Fernández Villegas <miguelfervi@gmail.com>
2017-06-21 11:45:08 +0200

    Subir entradas a Firebase

--- src/app/components/exploradores/explorador.component.ts
+++ src/app/components/exploradores/explorador.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
+import {Router} from "@angular/router";
 import {Explorador} from "../../interfaces/explorador.interface";
+import { ExploradoresService }from "../../services/exploradores.service";
 
 @Component({
   selector: 'app-explorador',
@@ -16,7 +18,8 @@ export class ExploradorComponent implements OnInit {
 
   }
 
-  constructor() { }
+  constructor(private _exploradoresService:ExploradoresService,
+              private router:Router) { }
 
   ngOnInit() {
   }
@@ -24,6 +27,14 @@ export class ExploradorComponent implements OnInit {
   guardar(){
 
     console.log(this.explorador);
+
+    this._exploradoresService.nuevoExplorador( this.explorador )
+        .subscribe( data=>{
+          this.router.navigate(['/explorador',data.name])
+
+        },
+        error=>console.error(error)
+      );
   }
 
 }
--- src/app/services/exploradores.service.ts
+++ src/app/services/exploradores.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from "@angular/http";
+import { Explorador } from "../interfaces/explorador.interface"
+import 'rxjs/Rx'
 
 @Injectable()
 export class ExploradoresService {
 
-  constructor() { }
+  exploradoresURL:string = "https://planetaria-4a540.firebaseio.com/exploradores.json"
+
+  constructor(private http:Http) { }
+
+  nuevoExplorador(explorador:Explorador){
+    let body = JSON.stringify( explorador );
+    let headers = new Headers({
+      'Content-Type':'application/json'
+    });
+    return this.http.post( this.exploradoresURL,body, { headers })
+                .map ( res=>{
+                  console.log(res.json());
+                  return res.json();
+                })
+  }
 
 }
